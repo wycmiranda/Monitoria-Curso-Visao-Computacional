db8b6f25444ace810ab80799f90df1f3f00357af
wycmiranda <wymiranda@outlook.com>
2022-06-01 23:17:59 -0400

    Criacao da funcao convolve1D com uso do cv::filter2D

--- Convolucao/Convolucao.cpp
+++ Convolucao/Convolucao.cpp
@@ -1,18 +1,36 @@
 #include<opencv2/opencv.hpp>
 #include <iostream>
 
+enum class conv { SAME, FULL };
+
+void convolve1D(cv::Mat src, cv::Mat& dst, cv::Mat kernel, conv type = conv::SAME) {
+
+    cv::flip(kernel, kernel, 1);
+    int border = CV_HAL_BORDER_CONSTANT;
+    cv::Point anchor(-1, -1);
+
+    if (type == conv::SAME)
+        cv::filter2D(src, dst, src.depth(), kernel, anchor, 0.0, border);
+    else if (type == conv::FULL) {
+        unsigned extra = kernel.cols - 1;
+        cv::copyMakeBorder(src, src, 0, 0, extra / 2, extra / 2, border, cv::Scalar(0));
+        cv::filter2D(src, dst, src.depth(), kernel, anchor, 0.0, border);
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    float src_data[] = {1, 3, 5, 4, 2};
-    float kernel_data[] = {3, 2, 1};
+    float src_data[] = { 1, 3, 5, 4, 2 };
+    float kernel_data[] = { 3, 2, 1 };
 
     cv::Mat src(1, 5, CV_32FC1, src_data);
     cv::Mat kernel(1, 3, CV_32FC1, kernel_data);
 
     cv::Mat result;
-    cv::filter2D(src, result, src.depth(), kernel);
+    //cv::filter2D(src, result, src.depth(), kernel);
+    convolve1D(src, result, kernel, conv::FULL);
 
     src.convertTo(src, CV_8UC1);
     kernel.convertTo(kernel, CV_8UC1);
@@ -31,5 +49,5 @@ int main()
     cv::destroyAllWindows();
 
     return 0;
-    
+
 }
