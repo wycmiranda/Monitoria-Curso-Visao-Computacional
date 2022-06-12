7e595ae94330982eae649ddf65690cb41c762a25
wycmiranda <wymiranda@outlook.com>
2022-06-12 09:44:56 -0400

    Adicao da convolucao 2D

--- Convolucao/Convolucao.cpp
+++ Convolucao/Convolucao.cpp
@@ -1,5 +1,5 @@
 #include<opencv2/opencv.hpp>
-#include <iostream>
+#include<opencv2/highgui/highgui.hpp>
 
 enum class conv { SAME, FULL };
 
@@ -18,28 +18,44 @@ void convolve1D(cv::Mat src, cv::Mat& dst, cv::Mat kernel, conv type = conv::SAM
     }
 }
 
+void convolve2D(cv::Mat src, cv::Mat& dst, cv::Mat kernel, conv type = conv::SAME) {
+
+    cv::flip(kernel, kernel, -1);
+    int border = CV_HAL_BORDER_CONSTANT;
+    cv::Point anchor(-1, -1);
+
+    if (type == conv::SAME)
+        cv::filter2D(src, dst, src.depth(), kernel, anchor, 0.0, border);
+    else if (type == conv::FULL) {
+        unsigned extra_rows = kernel.rows - 1;
+        unsigned extra_cols = kernel.cols - 1;
+        cv::copyMakeBorder(src, src, extra_rows/2, extra_rows/2, extra_cols / 2, extra_cols / 2, border, cv::Scalar(0));
+        cv::filter2D(src, dst, src.depth(), kernel, anchor, 0.0, border);
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    float src_data[] = { 1, 3, 5, 4, 2 };
-    float kernel_data[] = { 3, 2, 1 };
+    float src_data[] = { 2, 3, 1, 1, 7, 2, 1, 3, 0};
+    float kernel_data[] = {2, 0, 3, 1, 0, 1, 0, 2, 2};
 
-    cv::Mat src(1, 5, CV_32FC1, src_data);
-    cv::Mat kernel(1, 3, CV_32FC1, kernel_data);
+    cv::Mat src(3, 3, CV_32FC1, src_data);
+    cv::Mat kernel(3, 3, CV_32FC1, kernel_data);
 
     cv::Mat result;
     //cv::filter2D(src, result, src.depth(), kernel);
-    convolve1D(src, result, kernel, conv::FULL);
+    convolve2D(src, result, kernel, conv::FULL);
 
-    src.convertTo(src, CV_8UC1);
-    kernel.convertTo(kernel, CV_8UC1);
-    result.convertTo(result, CV_8UC1);
+    src.convertTo(src, CV_8U);
+    kernel.convertTo(kernel, CV_8U);
+    result.convertTo(result, CV_8U);
 
     std::string janelas[] = { "Imagem","Kernel", "Convolution" };
     cv::namedWindow(janelas[0], cv::WINDOW_GUI_NORMAL);
     cv::namedWindow(janelas[1], cv::WINDOW_GUI_NORMAL);
-    cv::namedWindow(janelas[2], cv::WINDOW_GUI_NORMAL);
+    cv::namedWindow(janelas[2], cv::WINDOW_NORMAL);
 
     cv::imshow(janelas[0], src);
     cv::imshow(janelas[1], kernel);
